fafd723619b571647b8e9fb973df1aae5b32a661
faizul <faizulcse@gmail.com>
2022-04-18 03:35:08 +0600

    add test cases

--- tests/suite3/test_LoginTest.py
+++ tests/suite3/test_LoginTest.py
@@ -4,3 +4,9 @@ from tests.BaseTest import BaseTest
 class LoginTest(BaseTest):
     def test_first(self):
         print("\n" + self.driver.title)
+
+    def test_second(self):
+        print("\n" + self.driver.title)
+
+    def test_third(self):
+        print("\n" + self.driver.title)
