2c06a61ec69edc7d8a8c5e8ae004e54bbf52b550
faizul <faizulcse@gmail.com>
2022-04-21 18:18:00 +0600

    add implicit wait 1

--- tests/BaseTest.py
+++ tests/BaseTest.py
@@ -8,6 +8,9 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         self.setup = DriverSetup()
         self.driver = self.setup.open_browser()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(1)
+        self.driver.get("https://www.google.com")
 
     def tearDown(self):
         self.setup.close_browser()
--- utils/DriverSetup.py
+++ utils/DriverSetup.py
@@ -3,22 +3,18 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.remote.remote_connection import RemoteConnection
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class DriverSetup(object):
     driver = None
 
     def open_browser(self):
-        url = "http://localhost:4444/wd/hub"
-        service = Service(r"./driver/chromedriver.exe")
-        opts = webdriver.ChromeOptions()
-        opts.add_argument("--headless")
-        if os.getenv("remote"):
-            self.driver = webdriver.Remote(RemoteConnection(url), options=opts)
-        else:
-            self.driver = webdriver.Chrome(service=service, options=opts)
-        self.driver.maximize_window()
-        self.driver.get("https://www.google.com")
+        remote_url = "http://localhost:4444/wd/hub"
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) \
+            if bool(os.getenv("remote")) is False else webdriver.Remote(RemoteConnection(remote_url), options=options)
         return self.driver
 
     def close_browser(self):
