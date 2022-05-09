ebb8b8d032ace9524b2f40c60d51b487c5117a76
faizul <faizulcse@gmail.com>
2022-05-09 17:46:30 +0600

    create latest setup file

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-"""Minimal setup file for SELENIUM-AUTOMATION-PYTHON"""
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='selenium-automation-python',
-    version='0.1.0',
-    description='Website UI testing using selenium-python',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    entry_points={},
-    # metadata
+    version='1.0.0',
+    packages=['src', 'src.pages', 'src.tests', 'src.utils'],
+    url='',
+    license='MIT license',
     author='Faizul Islam',
     author_email='faizulcse@gmail.com',
-    license='proprietary',
+    description='A prototype of selenium-automation-python',
     install_requires=['selenium', 'webdriver-manager', 'pytest', 'pytest-dotenv',
                       'pytest-xdist', 'allure-pytest', 'pytest-html']
 )
