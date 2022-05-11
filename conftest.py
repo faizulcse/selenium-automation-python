698ec13414008ec6d67f89c545aa83ff643107f3
faizul <faizulcse@gmail.com>
2022-05-11 17:02:55 +0600

    remove driver setup file

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
 import os
 
 import pytest
-
-from src.utils.driver_helper import get_driver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.remote.remote_connection import RemoteConnection
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 def pytest_addoption(parser):
@@ -13,9 +17,71 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(autouse=True)
-def driver(request):
+def current_driver(request):
     pytest.data = request.config
-    web_driver = get_driver()
-    yield web_driver
+    browser = request.config.getoption("--BROWSER").lower()
+    remote = request.config.getoption("--REMOTE").lower()
+
+    driver = get_driver(browser, remote)
+    driver.maximize_window()
+    driver.get(os.environ.get("BASE_URL"))
+    driver.implicitly_wait(os.environ.get("IMPLICIT_WAIT"))
+    yield
     if pytest.data.getoption("--DETACH") == "false" and driver is not None:
-        web_driver.quit()
+        driver.quit()
+
+
+def get_driver(browser, remote):
+    if remote == "true":
+        return remote_driver(browser)
+    else:
+        return local_driver(browser)
+
+
+def local_driver(browser):
+    match browser:
+        case "chrome":
+            ChromeDriverManager(log_level=0)
+            return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=get_options(browser))
+        case "firefox":
+            GeckoDriverManager(log_level=0)
+            return webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=get_options(browser))
+        case "edge":
+            EdgeChromiumDriverManager(log_level=0)
+            return webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=get_options(browser))
+        case _:
+            print("===============> Error!!! Please provide a valid browser name: " + browser)
+
+
+def remote_driver(browser):
+    return webdriver.Remote(RemoteConnection(os.getenv("HUB_URL")), options=get_options(browser))
+
+
+def get_options(browser):
+    headless = pytest.data.getoption("--HEADLESS").lower()
+    detach = pytest.data.getoption("--DETACH").lower()
+    match browser:
+        case "chrome":
+            options = webdriver.ChromeOptions()
+            if headless == "true":
+                options.add_argument('--headless')
+            if detach == "true":
+                options.add_experimental_option("detach", True)
+            return options
+
+        case "firefox":
+            options = webdriver.FirefoxOptions()
+            if headless == "true":
+                options.add_argument('--headless')
+            return options
+
+        case "edge":
+            options = webdriver.EdgeOptions()
+            if headless == "true":
+                options.add_argument('--headless')
+            if detach == "true":
+                options.add_experimental_option("detach", True)
+            return options
+
+        case _:
+            print("===============> Error!!! Please provide a valid browser name: " + browser)
--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -5,11 +5,11 @@ from src.pages.login_page import LoginPage
 
 class TestLogin:
     @pytest.mark.skip(reason="Skip by Tester")
-    def test_method_1(self, driver):
+    def test_method_1(self, current_driver):
         print("============testmethod_1============>")
 
     @pytest.mark.login
-    def test_method_2(self, driver):
-        login_page = LoginPage(driver)
+    def test_method_2(self, current_driver):
+        login_page = LoginPage(current_driver)
         print("Session Id: " + login_page.get_session_id())
         login_page.print_info()
